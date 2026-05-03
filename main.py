f5beac0b20550870b4f230916adf4bf66eb78a28
hjsui <moslim.elomari@gmail.com>
2026-05-03 02:26:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def keep_alive():
     t.start()
 
 # ------------------- إعدادات البوت -------------------
-API_TOKEN = '8325861290:AAEsIB4fiWPT7kGqVLVIpYNjDmll99CXm9M'
+API_TOKEN = 'BOT_TOKEN'
 ADMIN_ID = 6500854407
 bot = telebot.TeleBot(API_TOKEN)
 
