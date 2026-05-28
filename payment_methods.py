cf81aa33b8e89e837c8319238342ba1be4aa0cd3
hjsui <moslim.elomari@gmail.com>
2026-05-28 13:08:36 +0100

    Create payment_methods.py

---
+++ payment_methods.py
@@ -0,0 +1,26 @@
+PAYMENT_METHODS = {
+    "cih": {
+        "name_ar": "🏦 CIH BANK",
+        "name_en": "🏦 CIH BANK",
+        "details_ar": "الاسم: MOSLIM STORE\nRIB: <code>6899904211035500</code>\nR.I.P BANCAIRE: <code>230480689990421103550036</code>",
+        "details_en": "Name: MOSLIM STORE\nRIB: <code>6899904211035500</code>\nR.I.P: <code>230480689990421103550036</code>",
+    },
+    "barid": {
+        "name_ar": "🏦 BARID BANK",
+        "name_en": "🏦 BARID BANK",
+        "details_ar": "الاسم: MOSLIM ELOMARI\nرقم الحساب: <code>13958215</code>",
+        "details_en": "Name: MOSLIM ELOMARI\nAccount: <code>13958215</code>",
+    },
+    "cashplus": {
+        "name_ar": "🏦 CASH PLUS",
+        "name_en": "🏦 CASH PLUS",
+        "details_ar": "رقم الحساب: <code>0723644027</code>",
+        "details_en": "Account: <code>0723644027</code>",
+    },
+    "binance": {
+        "name_ar": "💰 Binance",
+        "name_en": "💰 Binance",
+        "details_ar": "𝙄𝘿: <code>1208575784</code>\n𝙉𝘼𝙈𝙀: مـتـجـــر مـسـلـــم",
+        "details_en": "ID: <code>1208575784</code>\nNAME: MOSLIM STORE",
+    }
+}
